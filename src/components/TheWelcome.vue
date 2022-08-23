1d84966175a74eaa821971268c8cf15cbd267c1c
RakaAaditya01 <raka27275@gmail.com>
2022-08-24 06:59:35 +0700

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,79 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+<div class="bungkus">
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Personal Profile</template>
+  Raka Aditya Sumarno
+  <br>
+  Tempat, tanggal lahir : Depok, 25 Mei 2005
+  <br>
+  Alamat : Gg. Guan No.36, Curug, Kec. Cimanggis, Kota Depok, Jawa Barat 16455
+  </WelcomeItem>
+
+  
+    <WelcomeItem>
+      <template #heading>Education</template>
+      2020-2023
+      <br />
+      <a href="https://smktarunabhakti.net/" target="_blank">SMK TARUNA BHAKTI, Depok</a>
+       <br />
+      FrontEnd Developer
+    </WelcomeItem>
+    <WelcomeItem>
+      <template #icon>
+        <EcosystemIcon />
+      </template>
+      <template #heading>Experience</template>
+    <ul>
+      <li>School Design</li>
+      <li>Hospital Design</li>
+      <li>Wedding Design</li>
+      <li>Food Delivery Design</li>
+    </ul>
+    </WelcomeItem>
+    <WelcomeItem>
+      <template #icon>
+        <CommunityIcon />
+      </template>
+      <template #heading>Language</template>
+    <ul>
+      <li>English</li>
+      <li>Indonesia</li>
+    </ul>
+    </WelcomeItem>
+    <WelcomeItem>
+      <template #icon>
+        <SupportIcon />
+      </template>
+      <template #heading>Contact</template>
+    <h4>
+      (+62) 895-3928-28575
+    </h4>
+    <h4>
+      raka27275@gmail.com
+    </h4>
+      <h4>Ini Adalah link Github saya</h4><a target="_blank" href="https://github.com/RakaAditya01">Raka Aditya</a>.
+    </WelcomeItem>
+  </div>
+</template>
+
+<style>
+  .bungkus{
+    display: flex;
+  }
+
+  ul li {
+    list-style: none;
+  }
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,164 @@
+<template>
+
+  <div class="about">
+    <h3>Saya seorang Front End Developer dengan
+pengetahuan menyeluruh tentang desain dan website. <br>
+Saya mampu bekerja di bawah tekanan, dan juga
+dapat bekerja secara individual maupun dalam tim. <br>
+Saya mampu berkomunikasi dengan baik dengan rekan
+tim, dan dapat mengatur waktu.</h3>
+<br>
+<h1>
+  My Skill
+</h1>
+<div class="box">
+  <ul>
+    <li>
+    <div class="lang"></div>
+  <p>HTML5</p>
+    <div class="bar-html">
+      
+       <div class="progress"></div>
+       <div class="bar-percent">
+        <span class="percent">80%</span>
+       </div>
+    </div>
+    </li>
+  </ul>
+
+  <ul>
+    <li>
+    <div class="lang"></div>
+  <p>CSS</p>
+    <div class="bar-html">
+      
+       <div class="progress"></div>
+       <div class="bar-percent-css">
+        <span class="percent-css">90%</span>
+       </div>
+    </div>
+    </li>
+  </ul>
+
+  <ul>
+    <li>
+    <div class="lang"></div>
+  <p>JAVASCRIPT</p>
+    <div class="bar-html">
+      
+       <div class="progress"></div>
+       <div class="bar-percent-java">
+        <span class="percent-java">50%</span>
+        </div>
+    </div>
+    </li>
+  </ul>
+  <ul>
+    <li>
+    <div class="lang"></div>
+  <p>VUE</p>
+    <div class="bar-html">
+      
+       <div class="progress"></div>
+       <div class="bar-percent-vue">
+        <span class="percent-vue">50%</span>
+        </div>
+    </div>
+    </li>
+  </ul>
+</div>
+</div>
+  
+</template>
+
+<script>
+ 
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+   
+    /* display: flex; */
+    align-items: center;
+  }
+  
+}
+
+.box{
+  align-items: center;
+  width: 800px;
+  padding: 40px;
+  background: #f3f3f3;
+  box-shadow: 3px 3px 10px 3px rgba(0, 0, 0, 0.1);
+}
+
+ul li{
+  list-style-type: none;
+  padding: 10px;
+}
+
+/* HTML */
+/* .lang{
+  margin: 5px;
+  font-size: 20px;
+} */
+
+.bar-html{
+  width: 100%;
+  background: #dfdfdf;
+  overflow: hidden;
+  padding: 5px;
+}
+
+.progress{
+  float: left;
+  padding: 5px;
+}
+
+.percent{
+  color: white;
+}
+
+.bar-percent{
+  width: 80%;
+  background-color: red;
+}
+/* END */
+/* CSS */
+.percent-css{
+  color: white;
+}
+
+.bar-percent-css{
+  width: 90%;
+  background-color: blue;
+}
+/* END */
+
+/* JAVASCRIPT */
+.percent-java{
+  color: white;
+  /* float: right; */
+}
+
+.bar-percent-java{
+  width: 50%;
+  background-color: blue;
+}
+/* END */
+
+/* VUE */
+.percent-vue{
+  color: white;
+  /* float: right; */
+}
+
+.bar-percent-vue{
+  width: 50%;
+  background-color: purple;
+}
+/* END */
+</style>
+
